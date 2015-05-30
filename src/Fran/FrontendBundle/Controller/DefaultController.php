906131a63731dca1393f7ef5b290971b7c5cc030
aiorian <firodriguez@emailn.de>
2015-05-30 12:34:43 -0400

    ADDED: Frontend

--- src/Fran/FrontendBundle/Controller/DefaultController.php
+++ src/Fran/FrontendBundle/Controller/DefaultController.php
@@ -3,11 +3,23 @@
 namespace Fran\FrontendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('FrontendBundle:Default:index.html.twig', array('name' => $name));
+        $em = $this->getDoctrine()->getManager();
+        $ofertas = $em->getRepository('BackendBundle:Oferta')->findBy(array('disponible'=>true));
+       try{
+           $dataResult = array();
+           foreach ($ofertas as $oferta) {
+               $dataResult[]=$oferta->NormalizedAssoc();
+           }
+           return new Response(json_encode($dataResult),200);
+       }catch (\Exception $e)
+       {
+           return new Response($e->getMessage(),500);
+       }
     }
 }
