1c1589a2afe7a3f63074f4068594fe9075f63486
aiorian <firodriguez@emailn.de>
2015-05-31 21:34:06 -0400

    Cleaning

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,12 +23,12 @@ class AppKernel extends Kernel
             new Vich\UploaderBundle\VichUploaderBundle(),
         );
 
-        if (in_array($this->getEnvironment(), array('dev', 'test'))) {
+   /*     if (in_array($this->getEnvironment(), array('dev', 'test'))) {
             $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-        }
+        }*/
 
         return $bundles;
     }
